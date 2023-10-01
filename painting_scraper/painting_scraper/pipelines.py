54185d5a6b79200e1abf590f438b0f4bdb454b5a
Mahshid Salour <Mahshid.Salour@uibt.com>
2023-10-01 22:36:56 +0200

    Pagination

--- painting_scraper/painting_scraper/pipelines.py
+++ painting_scraper/painting_scraper/pipelines.py
@@ -1,7 +1,9 @@
+from pymongo import MongoClient
 from openpyxl import Workbook
 from itemadapter import ItemAdapter
 
-
+     
+# Saving Data Locally to Excel
 class PaintingScraperPipeline:
     def open_spider(self, spider):
         self.workbook=Workbook()
@@ -14,4 +16,23 @@ class PaintingScraperPipeline:
         return item
 
     def close_spider(self, spider):
-        self.workbook.save("paintings.xlsx")    
+        self.workbook.save("paintings.xlsx")   
+
+        #Saving data to Mongo DB
+# class PaintingScraperPipeline:
+#     def open_spider(self, spider):
+#         self.client=MongoClient(
+#             host=""
+#             connect=False
+#         )
+#         self.collection=self.client.get_database("Painting").get_collection("Cityscape Art")
+
+#     def process_item(self, item, spider):
+#         self.collection.insert_one(
+#            # {"title":item['title'],"artist":item['artist'],"size":item['size']}
+#            ItemAdapter(item).asdict()
+#         )
+#         return item
+
+#     def close_spider(self, spider):
+#         self.client.close() 
--- painting_scraper/painting_scraper/spiders/painting.py
+++ painting_scraper/painting_scraper/spiders/painting.py
@@ -1,13 +1,17 @@
 import scrapy
 from painting_scraper.items import PaintingScraperItem
 from scrapy.loader import ItemLoader
+from urllib.parse import urljoin
 
 class PaintingSpider(scrapy.Spider):
     name = "painting"
     start_urls = ["https://www.californiawatercolor.com/collections/original-california-paintings-for-sale"]
     cols=["Title", "Artist","Size"]
-    
+    def __init__(self):
+        super().__init__()
+        self.page_count= 0
     def parse(self, response):
+        self.page_count += 1
         """
         Parse the response from the website and extract painting information.
 
@@ -26,7 +30,25 @@ class PaintingSpider(scrapy.Spider):
             loader.add_css('artist', "div.product-item-vendor::text")
             loader.add_css('size', "div.origsizecol::text")
             yield loader.load_item()
-            
+
+        print("[ PAGE COUNT]:",self.page_count)
+        pagination_div = response.css('div.pagination.wide')
+
+        if pagination_div:
+            # Extract the next page URL and its href
+            next_page_element = pagination_div.css('a:contains("Next →")')
+            if next_page_element:
+                next_page_url = next_page_element.xpath("@href").get()
+                full_next_page_url = urljoin(self.start_urls[0], next_page_url)
+                print("Next Page URL:", full_next_page_url)
+                yield scrapy.Request(url=full_next_page_url)
+                
+            else:
+                print("Next page not found.")
+        else:
+            print("Pagination div not found.")
+
+
             # painting_item=PaintingScraperItem()
             # painting_item['title'] = painting.css("a.coll-prod-title::text").get()
             # painting_item['artist'] = painting.css("div.product-item-vendor::text").get()
